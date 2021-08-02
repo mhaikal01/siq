d2f8554590ecfec7ccc934fa3d626d36c27b2e00
M Haikal <haikalmuhammad330@gmail.com>
2021-08-02 22:20:50 +0700

    inisialisasi firebase dan mengembalikan streamprovider

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:provider/provider.dart';
 import 'package:siq/model/savings.dart';
 import 'package:siq/pages/splash_page.dart';
+import 'package:siq/services/auth_services.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   await Hive.initFlutter();
   Hive.registerAdapter(SavingsAdapter());
   await Hive.openBox<Savings>('savings');
@@ -15,10 +19,14 @@ void main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: SplashPage(),
-      debugShowCheckedModeBanner: false,
+    return StreamProvider.value(
+      value: AuthServices.userStream,
+      initialData: null,
+      child: MaterialApp(
+        title: 'Material App',
+        home: SplashPage(),
+        debugShowCheckedModeBanner: false,
+      ),
     );
   }
 }
